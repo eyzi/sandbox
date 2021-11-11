779009cbb0d109bec41c907308f5e1aa2b1bf526
Eyzi <contact@eyzi.dev>
2021-11-11 10:23:48 -0500

    feat: Implement dummy module
    This implements a simple module that returns a constant value.
    Included is a simple test file as well as Github action workflow
    to run the test file on self-hosted runner when pushed.

---
+++ src/index.spec.ts
@@ -0,0 +1,9 @@
+import { expect } from "chai";
+import run from "./index";
+
+describe("run function", function () {
+  it("should return a constant value", async function () {
+    const value = await run();
+    expect(value).to.equal("Yes");
+  });
+});
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+const RETURN_VALUE = "Yes";
+
+async function run() {
+  return RETURN_VALUE;
+}
+
+export default run;
